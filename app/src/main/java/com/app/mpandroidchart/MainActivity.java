cf4e19de9a362c02f775e5adf3046e1fe1675390
Neeraj_Immanent <neeraj.boominfotech@gmail.com>
2018-06-06 12:17:38 +0530

    06June2018

--- app/src/main/java/com/app/mpandroidchart/MainActivity.java
+++ app/src/main/java/com/app/mpandroidchart/MainActivity.java
@@ -81,23 +81,37 @@ public class MainActivity extends AppCompatActivity {
         entries.add(new Entry(Integer.parseInt(getCount("2018-04-04 15:00:12")),150));
         LineDataSet dataSet = new LineDataSet(entries,"");
         LineData lineData = new LineData(dataSet);
-        //Custom dateset points
+//        //Custom dateset points
+//        dataSet.setColors(getResources().getColor(R.color.colorBlueGreenLightLight));
 //        dataSet.setValueTextColor(getResources().getColor(R.color.colorWhite));//change text color of points
 //        dataSet.setDrawValues(false);//disable data value point text
 //        dataSet.setColor(getResources().getColor(R.color.colorWhite));//line color
 //        dataSet.setCircleColor(getResources().getColor(R.color.colorWhite));//circle color
 //        dataSet.setCircleColorHole(getResources().getColor(R.color.colorBlueGreen));//inside circle color
-//        dataSet.setLineWidth(1f);
-        //Custom line graph
+//        dataSet.setCircleRadius(5f);//change circle radious
+//        dataSet.setCircleHoleRadius(3);//change inner circle radius
+//        dataSet.setLineWidth(2f);//line width
+//        //Custom line graph
 //        linechart.setTouchEnabled(false);//to remove on touch
 //        XAxis xAxis = linechart.getXAxis();
-//        xAxis.setGranularity(1);
-//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        if (((BaseActivity)getActivity()) != null) {
+//            xAxis.setTypeface(((BaseActivity)getActivity()).setTypeface());//font style of x axis
+//        }
+//        xAxis.setGridColor(getResources().getColor(R.color.colorBlueGreenLightLight));//change x axis line color
+//        xAxis.setGranularity(1);//vertical line
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//position of x axis text
 //        xAxis.setValueFormatter(new DayAxisValueFormatter());
 //        YAxis yAxisRight = linechart.getAxisRight();
 //        yAxisRight.setEnabled(false);//disable right side y axis text
-//        linechart.getAxisLeft().setTextColor(getResources().getColor(R.color.colorWhite));//change text color of y axis
-//        linechart.getXAxis().setTextColor(getResources().getColor(R.color.colorWhite));//change text color of x axis
+//        YAxis yAxis = linechart.getAxisLeft();
+//        if (((BaseActivity)getActivity()) != null) {
+//            yAxis.setTypeface(((BaseActivity)getActivity()).setTypeface());//font style of y axis
+//        }
+////        yAxis.setGridColor(getResources().getColor(R.color.colorBlueGreenLightLight));////change y axis line color
+////        linechart.getAxisLeft().setDrawGridLines(false);//remove horizontal line
+//        linechart.getXAxis().setDrawGridLines(false);//remove vertical line
+//        linechart.getAxisLeft().setTextColor(getResources().getColor(R.color.colorBlueGreenLightLight));//change text color of y axis
+//        linechart.getXAxis().setTextColor(getResources().getColor(R.color.colorBlueGreenLightLight));//change text color of x axis
 //        linechart.getLegend().setEnabled(false);//disable the bottom box
 //        linechart.getDescription().setEnabled(false);//disable the description label text
         linechart.setData(lineData);
